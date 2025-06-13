d63b35cdfb7712bc0cc622e14cf81bfa5c4374c3
Bryan-max95 <datasendnetworkbryan@gmail.com>
2025-06-13 03:14:13 -0500

    Update layout.tsx
    cddd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import './globals.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,8 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/d.png" type="/d.png" />
       </head>
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-black text-white`}>
+
         <Navbar />
         <main className="pt-16 min-h-screen">{children}</main>
         <Footer />
